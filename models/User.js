0400696aaf443dc194d5683bb48daa3c36888cf9
SDann <smdann17@gmail.com>
2022-11-05 12:00:13 -0500

    In the User.js model, created the userSchema including a function for validating an email address. Aslo created a virtual for friend count. Updated index.js for the models and added a Reaction model, which will be a subdocument schema in the Thought model.

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+
+// Function for validating a user's email address using valid email regex
+let emailValidation = function(email) {
+  const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return regex.test(email);
+};
+
+// User schema to create the User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [emailValidation, 'Enter a valid email address'],
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Enter a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.model.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    // Include virtuals in JSON response
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual for retrieving length of user's friends array
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
+
+  const User = model('User', userSchema);
+
+  module.exports = User;
